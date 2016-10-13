6395d6eff6bd7693a71c042c5566d0beef2a4fb7
Erik Timmers <e.timmers@gmail.com>
2016-10-13 17:08:43 +0200

    Make ISO 8601 the default temporal

--- src/decorator/temporal.js
+++ src/decorator/temporal.js
@@ -4,18 +4,27 @@ import {PersistentConfig, PropertyType} from '../persistent-config';
 import {Util} from '../util';
 
 export const TemporalFormat = Object.seal({
+  DEFAULT: 'YYYY-MM-DDTHH:mm:ssZ',
   DATETIME: 'YYYY-MM-DD HH:mm:ss',
   DATE: 'YYYY-MM-DD',
   TIME: 'HH:mm:ss'
 });
 
+const IS_TIMEZONE = /[+-][0-9]{2,2}:[0-9]{2,2}$/;
+
 const formats = Object.keys(TemporalFormat).map(key => TemporalFormat[key]);
+
+function parse(value, format) {
+  let parser = typeof value === 'string' && IS_TIMEZONE.test(value) ?
+      moment.parseZone : moment;
+  return parser(value, format);
+}
 
 export function Temporal(formatOrTarget, optPropertyKey, optDescriptor) {
   let isDecorator = Util.isPropertyDecorator(...arguments);
-  let format = TemporalFormat.DATETIME;
+  let format = TemporalFormat.DEFAULT;
   if (!isDecorator) {
-    format = formatOrTarget || TemporalFormat.DATETIME;
+    format = formatOrTarget || format;
     if (!formats.find(f => f === format)) {
       throw new Error(`invalid type for @Temporal() ${optPropertyKey}`);
     }
@@ -28,11 +37,11 @@ export function Temporal(formatOrTarget, optPropertyKey, optDescriptor) {
       type: PropertyType.TEMPORAL,
       getter: function() {
         let value = Reflect.apply(getter, this, []);
-        let val = moment(value, format);
+        let val = parse(value, format);
         return val.isValid() ? val : undefined;
       },
       setter: function(value) {
-        let val = moment(value, format);
+        let val = parse(value, format);
         if (!val.isValid()) {
           throw new Error(`invalid date: ${value}`);
         }
--- test/unit/decorator/temporal.spec.js
+++ test/unit/decorator/temporal.spec.js
@@ -4,8 +4,19 @@ import {Temporal, TemporalFormat} from '../../../src/decorator/temporal';
 import {PersistentData} from '../../../src/persistent-data';
 import {createEntityManagerStub} from '../helper';
 
+function getTimezoneOffset() {
+  let offset = new Date().getTimezoneOffset();
+  let H = Math.floor(-offset / 60);
+  let M = -offset % 60;
+  let pad = val => (val < 10 ? '0' : '') + val;
+  let prefix = offset <= 0 ? '+' : '';
+  return prefix + pad(H) + ':' + pad(M);
+}
+
+const TZ_OFFSET = getTimezoneOffset();
 const DATE = '2000-01-01';
 const DATETIME = '2000-01-01 12:00:00';
+const DEFAULT = '2000-01-01T12:00:00' + TZ_OFFSET;
 const TIME = '12:00:00';
 
 @Entity
@@ -17,6 +28,9 @@ class Foo {
   noFactory;
 
   @Temporal()
+  factory;
+
+  @Temporal(TemporalFormat.DEFAULT)
   default;
 
   @Temporal(TemporalFormat.DATE)
@@ -31,8 +45,8 @@ class Foo {
 
 function checkDateTime(entity, data, property) {
   expect(entity[property]).toBeUndefined();
-  entity[property] = DATETIME;
-  expect(data[property]).toEqual('2000-01-01 12:00:00');
+  entity[property] = DEFAULT;
+  expect(data[property]).toEqual('2000-01-01T12:00:00' + TZ_OFFSET);
   expect(entity[property].year()).toEqual(2000);
   expect(entity[property].month() + 1).toEqual(1);
   expect(entity[property].date()).toEqual(1);
@@ -58,6 +72,10 @@ describe('@Temporal', () => {
     expect(() => foo.noFactory = 'some day').toThrow();
   });
 
+  it('Factory', () => {
+    checkDateTime(foo, data, 'factory');
+  });
+
   it('Default', () => {
     checkDateTime(foo, data, 'default');
   });
@@ -72,7 +90,15 @@ describe('@Temporal', () => {
   });
 
   it('Date & time', () => {
-    checkDateTime(foo, data, 'dateTime');
+    expect(foo.dateTime).toBeUndefined();
+    foo.dateTime = DATETIME;
+    expect(data.dateTime).toEqual('2000-01-01 12:00:00');
+    expect(foo.dateTime.year()).toEqual(2000);
+    expect(foo.dateTime.month() + 1).toEqual(1);
+    expect(foo.dateTime.date()).toEqual(1);
+    expect(foo.dateTime.hour()).toEqual(12);
+    expect(foo.dateTime.minute()).toEqual(0);
+    expect(foo.dateTime.second()).toEqual(0);
   });
 
   it('Time', () => {
